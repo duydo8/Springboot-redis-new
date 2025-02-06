4aad415777f2fff33f99c76f6d65a7277b11ba20
duydv <duydo8>
2025-02-06 17:19:58 +0700

    refactor list and string redis

--- src/main/java/com/example/springboot/redis/controller/RedisListController.java
+++ src/main/java/com/example/springboot/redis/controller/RedisListController.java
@@ -1,8 +1,8 @@
 package com.example.springboot.redis.controller;
 
 import com.example.springboot.redis.service.RedisListService;
-import com.example.springboot.redis.service.RedisStringService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,49 +12,41 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/redis")
+@RequestMapping("/redis/list")
 public class RedisListController {
-    @Autowired
-    private RedisStringService redisStringService;
 
     @Autowired
     private RedisListService redisListService;
 
-    // Lưu dữ liệu vào Redis String
-    @PostMapping("/string/set")
-    public String setString(@RequestParam String key, @RequestParam String value) {
-        redisStringService.saveToString(key, value);
-        return "Value added to Redis String!";
-    }
-
-    // Lấy dữ liệu từ Redis String
-    @GetMapping("/string/get")
-    public Object getString(@RequestParam String key) {
-        return redisStringService.getFromString(key);
-    }
-
     // Thêm phần tử vào Redis List
-    @PostMapping("/list/add")
+    @PostMapping("/add")
     public String addToList(@RequestParam String key, @RequestParam Object value) {
         redisListService.addToList(key, value);
         return "Value added to Redis List!";
     }
 
     // Lấy tất cả phần tử trong Redis List
-    @GetMapping("/list/getAll")
+    @GetMapping("/getAll")
     public List<Object> getAllFromList(@RequestParam String key) {
         return redisListService.getAllFromList(key);
     }
 
     // Lấy phần tử đầu tiên trong Redis List
-    @GetMapping("/list/getFirst")
+    @GetMapping("/getFirst")
     public Object getFirstFromList(@RequestParam String key) {
         return redisListService.getFirstFromList(key);
     }
 
     // Lấy phần tử cuối cùng trong Redis List
-    @GetMapping("/list/getLast")
+    @GetMapping("/getLast")
     public Object getLastFromList(@RequestParam String key) {
         return redisListService.getLastFromList(key);
+    }
+
+    // Lấy phần tử cuối cùng trong Redis List
+    @DeleteMapping("/delete")
+    public Object removeFromList(@RequestParam String key, @RequestParam Object value) {
+        redisListService.removeFromList(key, value);
+        return "Value removed from Redis List!";
     }
 }
--- src/main/java/com/example/springboot/redis/controller/RedisStringController.java
+++ src/main/java/com/example/springboot/redis/controller/RedisStringController.java
@@ -1,4 +1,43 @@
 package com.example.springboot.redis.controller;
 
+import com.example.springboot.redis.service.RedisStringService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/redis/string")
 public class RedisStringController {
+    @Autowired
+    private RedisStringService redisStringService;
+
+    // Lưu dữ liệu vào Redis String
+    @PostMapping("/set")
+    public String setString(@RequestParam String key, @RequestParam String value) {
+        redisStringService.saveToString(key, value);
+        return "Value added to Redis String!";
+    }
+
+    // Lấy dữ liệu từ Redis String
+    @GetMapping("/get")
+    public Object getString(@RequestParam String key) {
+        return redisStringService.getFromString(key);
+    }
+
+    // Lưu dữ liệu vào Redis String
+    @DeleteMapping("/delete")
+    public String delete(@RequestParam String key) {
+        redisStringService.deleteFromString(key);
+        return "DELETE key successfully!";
+    }
+
+    // Lấy dữ liệu từ Redis String
+    @GetMapping("/hasKey")
+    public Object hasKey(@RequestParam String key) {
+        return redisStringService.hasKey(key);
+    }
 }
